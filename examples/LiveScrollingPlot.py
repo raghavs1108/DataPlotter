112bf4f9f9f332e74f91fb5f2fc51255d3d52f0c
cdac <karthiv.sw@gmail.com>
2015-07-08 11:37:41 +0530

    socket Server using capture command

--- examples/LiveScrollingPlot.py
+++ examples/LiveScrollingPlot.py
@@ -3,6 +3,7 @@ import initExample ## Add path to library (just for examples; you do not need th
 import pyqtgraph as pg
 from pyqtgraph.Qt import QtCore, QtGui
 import numpy as np
+import time
 
 win = pg.GraphicsWindow()
 win.setWindowTitle('Live Scrolling Plots')
@@ -51,6 +52,7 @@ yVal5 = []
 yVal6 = []
 
 curve1 = p5.plot(pen = "w")
+curve4 = p5.plot(pen = "r")
 curve2 = p6.plot(pen = "w")
 curve3 = p7.plot(pen = "w")
 # curve4 = p5.plot(pen = "y")
@@ -60,84 +62,100 @@ curve3 = p7.plot(pen = "w")
 curves.append(curve1)
 curves.append(curve2)
 curves.append(curve3)
+curves.append(curve4)
 # curves.append(curve4)
 # curves.append(curve5)
 # curves.append(curve6)
 
+captured = False
+maxCaptureDataCount = 2000
+def captureDataToFile():
+	global maxCaptureDataCount, captured, curve1, curve2, curve3, curve4, p5, ptr5, curves, xVal, yVal1, yVal2, yVal3, yVal4, yVal5, yVal6, startTime, maxDataCount
+	d = time.strftime("%d:%m:%Y")
+	t = time.strftime("%H:%M:%S")
+	s = "file"+d+";"+t+".txt"
+	string = "";
+	f1 = open(s, "w")
+	counter = 0
+	while counter < maxCaptureDataCount:
+		try:
+			string = "0,0,0,"+str(yVal1[-(maxCaptureDataCount -counter)])+","
+			string += str(yVal2[-(maxCaptureDataCount -counter)])+","
+			string += str(yVal3[-(maxCaptureDataCount -counter)])+","
+		except Exception as e:
+			print counter
+		string += "0,0,0,"
+		string += "0,0,0,"
+		string += str(xVal[-(maxCaptureDataCount -counter)])+"\n"	
+		print "\n\n\n\n\n"+string + "\n\n\n\n\n"
+		f1.write(string)
+		counter += 1
+	f1.close()
+	captured = True
+	print "file write complete.\n\n\n\n\n"
+
 def update():
+	global captured, curve1, curve2, curve3, curve4, p5, ptr5, curves, xVal, yVal1, yVal2, yVal3, yVal4, yVal5, yVal6, startTime, maxDataCount
+	f = open("stepdata.txt", "r+");
+	d = f.readline()
     
-    
-    global curve1, curve2, curve3, p5, ptr5, curves, xVal, yVal1, yVal2, yVal3, yVal4, yVal5, yVal6, startTime, maxDataCount
-    f = open("data.txt", "r+");
-    d = f.readline()
-    
-    while d:
-        values = d.split(",")
-        try:
-        	xAcc = float(values[3])
-        except Exception as e:
-        	print "values[3]: " + str(values)
-       	try:
-        	yAcc = float(values[4])
-        except Exception as e:
-        	print "values[4]: " + str(values)
-        try:
-        	zAcc = float(values[5])
-        except Exception as e:
-        	print "values[5]: " + str(values)
-        try:
-        	phonexAcc = float(values[6])
-        except Exception as e:
-        	print "values[6]: " + str(values)
-        try:
-        	phoneyAcc = float(values[7])
-        except Exception as e:
-        	print "values[7]: " + str(values)
-        try:
-        	phonezAcc = float(values[8])
-        except Exception as e:
-        	print "values[8]: " + str(values)
-        try:
-        	datetime = values[12]
-	        date = datetime.split(" ")[0]
-	        time = datetime.split(" ")[1]
+	while d:
+		if d == "capture\n" and captured == False:
+			print "\n\nCAPTURE RECIEVED!!\n\n\n"
+			captureDataToFile()
+		else:
+			captured = False
+			values = d.split(",")
+			try:
+				xAcc = float(values[3])
+				yAcc = float(values[4])
+				zAcc = float(values[5])
+				phonexAcc = float(values[6])
+				phoneyAcc = float(values[7])
+				phonezAcc = float(values[8])
+				datetime = values[12]
+				date = datetime.split(" ")[0]
+				time = datetime.split(" ")[1]
 
-	        hours = int(time.split(":")[0])
-	        minutes = int(time.split(":")[1])
-	        temp = time.split(":")[2]
-	        seconds = int(temp.split(".")[0])
-	        milliseconds = float(temp.split(".")[1])
-	        milliseconds = (milliseconds)/1000
+				hours = int(time.split(":")[0])
+				minutes = int(time.split(":")[1])
+				temp = time.split(":")[2]
+				seconds = int(temp.split(".")[0])
+				milliseconds = float(temp.split(".")[1])
+				milliseconds = (milliseconds)/1000
+				
+				time = float((hours*3600)+(minutes*60)+seconds)
+				now = time + milliseconds
+			except Exception as e:
+				print "Exception!: " + str(values)
+			# print now
+			for c in curves:
+				c.setPos(-(now-startTime), 0)
 	        
-	        time = float((hours*3600)+(minutes*60)+seconds)
-	        now = time + milliseconds
-        except Exception as e:
-        	print "values[12]: " + str(values)
-        # print now
-        for c in curves:
-            c.setPos(-(now-startTime), 0)
-        
-        xVal.append(now)
+			xVal.append(now)
 
-        yVal1.append(phonexAcc)
-        yVal2.append(phoneyAcc)
-        yVal3.append(zAcc)
-        ptr5 += 1
-        d = f.readline()
+			yVal1.append(phonexAcc)
+			yVal2.append(phoneyAcc)
+			yVal3.append(zAcc)
+			yVal4.append(3)
+			ptr5 += 1
+			d = f.readline()
 
-    f.truncate()
-    f.close()
-    try:
-        curve1.setData(x=xVal, y=yVal1)
-        curve2.setData(x=xVal, y=yVal2)
-        curve3.setData(x=xVal, y=yVal3)
-    except Exception as e:
-        print e
-    if len(xVal) > maxDataCount:
-        xVal = xVal[1:]
-        yVal1 = yVal1[1:]
-        yVal2 = yVal2[1:]
-        yVal3 = yVal3[1:]
+	f.truncate()
+	f.close()
+	try:
+		curve1.setData(x=xVal, y=yVal1)
+		curve2.setData(x=xVal, y=yVal2)
+		curve3.setData(x=xVal, y=yVal3)
+		curve4.setData(x=xVal, y=yVal4)
+	except Exception as e:
+		print e
+	if len(xVal) > maxDataCount:
+		xVal = xVal[1:]
+		yVal1 = yVal1[1:]
+		yVal2 = yVal2[1:]
+		yVal3 = yVal3[1:]
+		yVal4 = yVal4[1:]
 
 timer = pg.QtCore.QTimer()
 timer.timeout.connect(update)
--- examples/SocketServer.py
+++ examples/SocketServer.py
@@ -1,32 +1,51 @@
 import socket
 import sys
- 
+import traceback
+
 HOST = '10.182.0.111'   # Symbolic name, meaning all available interfaces
-PORT = 8000 # Arbitrary non-privileged port
- 
+PORT = 8080 # Arbitrary non-privileged port
+
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
 print 'Socket created'
 #Bind socket to local host and port
 try:
-    s.bind((HOST, PORT))
+	s.bind((HOST, PORT))
 except socket.error as msg:
-    print 'Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
-    sys.exit()
-print 'Socket bind complete'
+	print 'Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
+	sys.exit()
+	print 'Socket bind complete'
 #Start listening on socket
 s.listen(10)
 print 'Socket now listening'
- 
+
 #now keep talking with the client
 while 1:
-    #wait to accept a connection - blocking call
-    conn, addr = s.accept()
-    print 'Connected with ' + addr[0] + ':' + str(addr[1])
-    data = conn.recv(5000)
-    conn.send("recieved")
-    print data
-    conn.close()
+	#wait to accept a connection - blocking call
+	conn, addr = s.accept()
+	print 'Connected with ' + addr[0] + ':' + str(addr[1])
+	data = conn.recv(20000)
+	conn.send("recieved")
+	if data == "capture" or data == "[capture\n]" or data == "['capture\n']":
+		f = open("stepdata.txt", "w")
+		f.write(data+"\n")
+		f.close()
+		print "capture recieved.\n"
+	else:		    	
+		data = data.split(";")
+			# print data
 
+		f = open("stepdata.txt", "w")
+		for i in data:
+			try:
+				x = i.split("#")[0]
+				y = i.split("#")[1]
+				f.write(y+"\n")
+			except Exception as ex:
+				template = "An exception of type {0} occured. Arguments:\n{1!r}"
+				message = template.format(type(ex).__name__, ex.args)
+				print message
+		f.close()
+	conn.close()
 s.close()
